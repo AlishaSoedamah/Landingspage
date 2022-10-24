86e148326c87afb36d3c1f21bfdcd0f0242afc1b
=Alisha Soedamah <alisha.soedamah@gmail.com>
2022-10-24 23:25:31 +0200

    Finally finished the footer (responsive)

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,22 +3,23 @@ import { Link } from "gatsby";
 
 const Footer = () => (
   <footer className="container">
-      <h1>Stay in touch 👋</h1>  
-      <p>Send us an email</p>
-      <h2><Link to="/">fakemail@gmail.com</Link></h2>
-      <div className="inner-footer">
-        <div className="inner-footer__office">
-            <p className="extra-deco">Office</p>
-            <Link to="/">amsterdam</Link>
-        </div>
-        <div className="inner-footer__socials">
-            <p className="extra-deco">Socials</p>
-            <Link to="/">instagram</Link>
-            <Link to="/">twitter</Link>
-            <Link to="/">behance</Link>
-        </div>
+    <h1>Stay in touch 👋</h1>
+    <h2 className="email"><Link to="/">fakemail@gmail.com</Link></h2>
+    <div className="inner-footer">
+      <div className="inner-footer__office">
+        <p className="extra-deco">Office</p>
+        <Link to="/">amsterdam, the netherlands</Link>
+        <p className="extra-deco">time in office</p>
+        <p>{new Date().getHours() + ":" + new Date().getMinutes()}</p>
       </div>
-      <span>All rights reserved 2022</span>
+      <div className="inner-footer__socials">
+        <p className="extra-deco">Socials</p>
+        <Link to="/">instagram</Link>
+        <Link to="/">twitter</Link>
+        <Link to="/">behance</Link>
+      </div>
+    </div>
+    <p className="copyright" >All rights reserved {new Date().getFullYear()} &copy;</p>
   </footer>
 )
 
